b8aa906dfc7d465ada9a06299ef728340e7557de
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-08-31 11:23:06 +0500

    Introduce factor parameter in yield_calculation function
    The yield_calculation function now accepts a frequency  parameter with a default value, allowing for more flexible yield calculations.

--- Bond Calculators/Bond Yield Calculator.py
+++ Bond Calculators/Bond Yield Calculator.py
@@ -1,8 +1,9 @@
 from scipy.optimize import root_scalar
+import numpy as np
 
-def yield_calculation(C, r):
+def yield_calculation(C, r, f=1):
     equation = lambda r: sum(C / (1 + r) ** np.arange(len(C)))
     result = root_scalar(equation, bracket=[0, 1], args=(), method='bisect')
-    return result.root * 2
+    return round(result.root * f, 4)
   
-yield_calculation(C=[-98.39, 3, 3, 3, 103], r=0.05)
+yield_calculation(C=[-98.39, 3, 3, 3, 103], r=0.05, f=2)
