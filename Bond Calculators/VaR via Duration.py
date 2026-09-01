b78224b373fd0f88f507b433508f3a3982f38e67
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-09-01 11:19:39 +0500

    Refactor VaR_D function for clarity and accuracy

--- Bond Calculators/VaR via Duration.py
+++ Bond Calculators/VaR via Duration.py
@@ -1,11 +1,11 @@
 import numpy as np
 from scipy.stats import norm
 
-def VaR_D(P, C, r, y, f = 1, s = 1, VaR = 95):
+def VaR_D(P, C, r, y, f = 1, s = 1, VaR = 95): # Value-at-Risk for Bond
     
     B = P*(C/r * (1 - (1 + r/f) ** -(y*f)) + (1 + r/f) ** -(y*f)) # Bond Price
 
-    P_ = P * (1 + C / f) / (1 + r / f) ** (y * f)  # Principle Part
+    PR = P * (1 + C / f) / (1 + r / f) ** (y * f)  # Principle Part
     PV = []
     payments = []
     
@@ -13,10 +13,8 @@ def VaR_D(P, C, r, y, f = 1, s = 1, VaR = 95):
         PV.append(C * P / f / (1 + r / f) ** (n * f))  # NPV
         payments.append(n * PV[n - 1])  # Coupon PV
 
-    D = (sum(payments) + P_ * y * f) / (P_ + sum(PV))  # Duration
+    D = (sum(payments) + PR * y * f) / (PR + sum(PV))  # Duration
     
-    var_result = B * norm.ppf(1 - VaR*.01)*(D/(1 + (r - s*.01)/f))*(r/f) # VaR
-    return var_result
+    return B * norm.ppf(1 - VaR*.01) * (D/(1 + (r - s*.01)/f)) * (r/f) # VaR
 
-var_result = VaR_D(1000, 0.1, 0.05, 3, 1, 1) # Test
-print(var_result)
+VaR_D(1000, 0.1, 0.05, 3, 1, 1) # Test
