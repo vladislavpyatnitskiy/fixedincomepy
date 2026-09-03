51013e60cefb80a616f596c76a9de9c3f38bb6b6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-09-03 11:00:34 +0500

    Introduce scaling factor to IRR calculation
    The IRR function now includes a scaling factor that modifies the output, allowing for more flexible calculations.

--- IRR.py
+++ IRR.py
@@ -1,8 +1,13 @@
+import numpy as np
 from scipy.optimize import root_scalar
 
-def IRR(C, r):
-    equation = lambda r: sum(C / (1 + r) ** np.arange(len(C)))
-    result = root_scalar(equation, bracket=[0, 1], args=(), method='bisect')
-    return result.root
+def IRR(C, r, f=1):
+    
+    return root_scalar(
+      lambda r: sum(C / (1 + r) ** np.arange(len(C))), 
+      bracket=[0, 1], 
+      args=(), 
+      method='bisect'
+      ).root * f
   
 IRR(C=[-1000, 250, 300, 360, 432], r=.05)
